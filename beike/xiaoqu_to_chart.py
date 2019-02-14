62d3f08ca788a4a449b0cbd1bd28d1cee5ec42f4
xuming06 <507153809@qq.com>
2019-02-14 19:26:54 +0800

    add map and page.

--- beike/xiaoqu_to_chart.py
+++ beike/xiaoqu_to_chart.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append("../")
 
 import pandas as pd
-from pyecharts import Bar, Grid
+from pyecharts import Bar, Map, Page
 
 from beike.config import SPIDER_NAME
 from beike.place.city import cities
@@ -24,9 +24,11 @@ if __name__ == '__main__':
     xiaoqu_city_path = "{0}/{1}/xiaoqu".format(DATA_PATH, SPIDER_NAME)
     for ct in cities:
         try:
+            # ct = 'wh'
             # 注意，已经将分割符号转换成分号，因为有的小区名中有逗号
             df = pd.read_csv(xiaoqu_city_path + "/{0}.csv".format(ct), encoding="utf-8", sep=";")
-
+            dist = [i + "区" if not i.endswith("区") else i for i in df["district"].tolist()]
+            df.insert(0, "district_full", dist)
             # 打印总行数
             print("before: row number is {0}".format(len(df)))
             # 过滤房价为0的无效数据
@@ -39,41 +41,60 @@ if __name__ == '__main__':
             df.sort_values("price", ascending=False, inplace=True)
             num = -1
             print(df.head(10))
-
             city = cities[ct]
             xqs = df["xiaoqu"][0:num]
             prices = df["price"][0:num]
-            bar = Bar("{0}小区均价排名".format(city))
-            bar.add("小区均价排名", xqs, prices, is_label_show=True, is_more_utils=True,
-                    xaxis_interval=0,
-                    xaxis_rotate=30,
-                    is_datazoom_show=True,
-                    datazoom_type="both",
-                    datazoom_range=[10, 10.3],
-                    )
+            page = Page()
+            xiaoqu_bar = Bar("{0}小区均价".format(city), width=1200)
+            xiaoqu_bar.add("小区均价排名", xqs, prices,
+                           is_label_show=True,
+                           is_more_utils=True,
+                           xaxis_interval=0,
+                           xaxis_rotate=30,
+                           is_datazoom_show=True,
+                           datazoom_type="both",
+                           datazoom_range=[10, 10.3],
+                           )
+            xiaoqu_bar.render(path="html/{0}_xiaoqu_bar.html".format(ct))
 
             ####################################################
             # 区县均价排名
             ####################################################
-            district_df = df.groupby('district').mean()
+            district_df = df.groupby('district_full').mean()
             district_df = district_df.round(0)
             district_df.sort_values("price", ascending=False, inplace=True)
             print(district_df)
             districts = district_df.index
             prices = district_df["price"]
-            district_bar = Bar("{0}区县均价".format(city), title_top="50%")
-            district_bar.add("区县均价排名", districts, prices, is_stack=False,
+            district_bar = Bar("{0}区县均价".format(city), width=1200)
+            district_bar.add("区县均价排名", districts, prices,
+                             is_stack=False,
                              xaxis_interval=0,
                              xaxis_rotate=30,
                              mark_line=["average"],
-                             is_label_show=True, is_more_utils=True, legend_top="50%")
-            grid = Grid(height=720)
-            grid.add(bar, grid_bottom="60%")
-            grid.add(district_bar, grid_top="60%")
-            grid.render(path="html/{0}.html".format(ct))
-            # url = "http://localhost:8080/{0}.html".format(ct)
-            # web.open(url, new=0, autoraise=True)
-            # print(url)
+                             is_label_show=True,
+                             is_more_utils=True
+                             )
+            district_bar.render(path="html/{0}_district_bar.html".format(ct))
+
+            ####################################################
+            # 区县均价排名-地图
+            ####################################################
+            district_map = Map("{0}区县均价分布".format(city), width=1200, height=600)
+            district_map.add("", districts, prices,
+                             maptype=city,
+                             is_visualmap=True,
+                             visual_text_color="#000",
+                             visual_range=[5000, 25000],
+                             is_label_show=True,
+                             is_more_utils=True)
+            district_map.render(path="html/{0}_district_map.html".format(ct))
+
+            page.add(district_map)
+            page.add(district_bar)
+            page.add(xiaoqu_bar)
+            page.render(path="html/{0}.html".format(ct))
+            # break
         except Exception as e:
             print("error with df: ", e)
             continue
